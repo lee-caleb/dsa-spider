f7b70ac8e06d7dd32a2d9d772d1abac80ae89f7d
Lee CQ <47050568+lee-cq@users.noreply.github.com>
2023-06-07 10:32:25 +0800

    fix: DSA workflow

--- settings.py
+++ settings.py
@@ -5,11 +5,11 @@ from dsa_api import DSAClient
 
 __all__ = ['dsa_client', 'DSA_CONFIG']
 
-DSA_HTTP_HOST = os.getenv('DSA_HTTP_HOST', 'http://localhost:8000')
+DSA_HTTP = os.getenv('DSA_HTTP', 'http://localhost:8000')
 DSA_AUTH = os.getenv('DSA_AUTH')
 DSA_CONFIG = os.getenv('DSA_CONFIG')
 
 if len(sys.argv) >= 2:
     DSA_CONFIG = sys.argv[1]
 
-dsa_client = DSAClient(DSA_HTTP_HOST, DSA_AUTH)
+dsa_client = DSAClient(DSA_HTTP, DSA_AUTH)
